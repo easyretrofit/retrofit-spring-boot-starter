6109c6a0e1933183dc8f3317922dc45c235ec401
liuziyuan <liuziyuan.room@gmail.com>
2021-12-24 17:30:35 +0800

    base

---
+++ src/main/java/com/github/liuziyuan/retrofit/RetrofitSpringBootStarterApplication.java
@@ -0,0 +1,13 @@
+package com.github.liuziyuan.retrofit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetrofitSpringBootStarterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RetrofitSpringBootStarterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/liuziyuan/retrofit/RetrofitSpringBootStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.liuziyuan.retrofit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RetrofitSpringBootStarterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
