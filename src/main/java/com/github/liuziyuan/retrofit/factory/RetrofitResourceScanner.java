b019cd3068534be088b2b561a918c2cf016e9e2f
liuziyuan <liuziyuan.room@gmail.com>
2022-01-05 18:20:06 +0800

    add scan function

--- src/main/java/com/github/liuziyuan/retrofit/factory/RetrofitResourceScanner.java
+++ src/main/java/com/github/liuziyuan/retrofit/factory/RetrofitResourceScanner.java
@@ -1,11 +1,8 @@
 package com.github.liuziyuan.retrofit.factory;
 
 import com.github.liuziyuan.retrofit.annotation.RetrofitBuilder;
-import com.github.liuziyuan.retrofit.annotation.RetrofitInterceptor;
 import org.reflections.Reflections;
 import org.reflections.util.ConfigurationBuilder;
-import org.springframework.core.env.Environment;
-import org.springframework.core.io.ResourceLoader;
 
 import java.util.Arrays;
 import java.util.Set;
@@ -18,21 +15,19 @@ import java.util.stream.Collectors;
  */
 public class RetrofitResourceScanner {
 
-    private Environment environment;
-    private ResourceLoader resourceLoader;
-    private Set<Class<?>> typesAnnotatedWithRetrofitBuilder;
-    private Set<Class<?>> typesAnnotatedWithRetrofitInterceptor;
-
-
-    public void doScan(String... basePackages) {
-        Pattern filterPattern = Pattern.compile(Arrays.stream(basePackages)
-                .map(s -> s.replace(".", "/"))
-                .collect(Collectors.joining("|", "(", ").*?")));
-        ConfigurationBuilder configuration = new ConfigurationBuilder().forPackages(basePackages).filterInputsBy(s -> filterPattern.matcher(s).matches());
+    public Set<Class<?>> doScan(String... basePackages) {
+        ConfigurationBuilder configuration;
+        if (basePackages.length == 0) {
+            configuration = new ConfigurationBuilder().forPackages("");
+        } else {
+            Pattern filterPattern = Pattern.compile(Arrays.stream(basePackages)
+                    .map(s -> s.replace(".", "/"))
+                    .collect(Collectors.joining("|", "(", ").*?")));
+            configuration = new ConfigurationBuilder().forPackages(basePackages).filterInputsBy(s -> filterPattern.matcher(s).matches());
+        }
         Reflections reflections = new Reflections(configuration);
-        typesAnnotatedWithRetrofitBuilder = reflections.getTypesAnnotatedWith(RetrofitBuilder.class);
-        typesAnnotatedWithRetrofitInterceptor = reflections.getTypesAnnotatedWith(RetrofitInterceptor.class);
-        return;
+        return reflections.getTypesAnnotatedWith(RetrofitBuilder.class);
     }
+
 
 }
--- src/test/java/com/github/liuziyuan/retrofit/RetrofitTestApplication.java
+++ src/test/java/com/github/liuziyuan/retrofit/RetrofitTestApplication.java
@@ -3,13 +3,13 @@ package com.github.liuziyuan.retrofit;
 import com.github.liuziyuan.retrofit.annotation.EnableRetrofit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import retrofit2.Retrofit;
 
 /**
  * @author liuziyuan
  * @date 12/24/2021 5:51 PM
  */
-@EnableRetrofit("com.github.liuziyuan.retrofit.demo.api")
+//@EnableRetrofit("com.github.liuziyuan.retrofit.demo.api")
+@EnableRetrofit
 @SpringBootApplication
 public class RetrofitTestApplication {
     public static void main(String[] args) {
