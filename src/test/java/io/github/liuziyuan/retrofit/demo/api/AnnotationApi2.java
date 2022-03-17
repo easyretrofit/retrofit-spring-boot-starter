08ac9af6f7d1a97ceeaeb2b69aba084e2ff64eec
liuziyuan <liuziyuan.room@gmail.com>
2022-03-17 10:02:48 +0800

    fix @RetrofitBase inherit issue

--- src/test/java/io/github/liuziyuan/retrofit/demo/api/AnnotationApi2.java
+++ src/test/java/io/github/liuziyuan/retrofit/demo/api/AnnotationApi2.java
@@ -1,10 +1,13 @@
 package io.github.liuziyuan.retrofit.demo.api;
 
 import io.github.liuziyuan.retrofit.annotation.RetrofitBase;
+import io.github.liuziyuan.retrofit.annotation.RetrofitInterceptor;
+import io.github.liuziyuan.retrofit.demo.MyRetrofitInterceptor3;
 
 /**
  * @author liuziyuan
  */
 @RetrofitBase(baseApi = AnnotationApi.class)
+@RetrofitInterceptor(handler = MyRetrofitInterceptor3.class)
 public interface AnnotationApi2 {
 }
