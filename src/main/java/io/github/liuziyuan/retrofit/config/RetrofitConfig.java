664b92caabee1dac872779f54a4378f76915399a
liuziyuan <liuziyuan.room@gmail.com>
2022-03-30 14:43:51 +0800

    @Import RetrofitConfig.class in EnableRetrofit Annotation

---
+++ src/main/java/io/github/liuziyuan/retrofit/config/RetrofitConfig.java
@@ -0,0 +1,19 @@
+package io.github.liuziyuan.retrofit.config;
+
+import io.github.liuziyuan.retrofit.RetrofitResourceDefinitionRegistry;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author liuziyuan
+ */
+@Configuration
+public class RetrofitConfig {
+
+    @Bean
+    @ConditionalOnMissingBean
+    public static RetrofitResourceDefinitionRegistry retrofitResourceDefinitionRegistry() {
+        return new RetrofitResourceDefinitionRegistry();
+    }
+}
