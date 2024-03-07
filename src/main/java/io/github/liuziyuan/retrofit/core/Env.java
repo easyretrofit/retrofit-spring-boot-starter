b2580e7e82e8593b3812c93e8ad157b77cd8e984
liuziyuan <liuziyuan.room@gmail.com>
2024-03-07 12:37:58 +0800

    remove core code

--- src/main/java/io/github/liuziyuan/retrofit/core/Env.java
+++
@@ -1,6 +0,0 @@
-package io.github.liuziyuan.retrofit.core;
-
-public interface Env {
-
-    String resolveRequiredPlaceholders(String text);
-}
--- src/main/java/io/github/liuziyuan/retrofit/core/annotation/InterceptorType.java
+++
@@ -1,8 +0,0 @@
-package io.github.liuziyuan.retrofit.core.annotation;
-
-/**
- * @author liuziyuan
- */
-public enum InterceptorType {
-    DEFAULT, NETWORK
-}
