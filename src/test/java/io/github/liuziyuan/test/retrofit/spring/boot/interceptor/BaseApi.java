c5c45abaae4a8a991676187b716ad4bf34a7243b
liuziyuan <liuziyuan.room@gmail.com>
2024-03-08 14:25:24 +0800

    UT test

--- src/test/java/io/github/liuziyuan/test/retrofit/spring/boot/interceptor/BaseApi.java
+++
@@ -1,8 +0,0 @@
-package io.github.liuziyuan.test.retrofit.spring.boot.interceptor;
-
-import io.github.liuziyuan.retrofit.core.annotation.RetrofitBuilder;
-import io.github.liuziyuan.retrofit.core.annotation.RetrofitInterceptor;
-
-@RetrofitBuilder(denyGlobalConfig = true)
-public interface BaseApi {
-}
