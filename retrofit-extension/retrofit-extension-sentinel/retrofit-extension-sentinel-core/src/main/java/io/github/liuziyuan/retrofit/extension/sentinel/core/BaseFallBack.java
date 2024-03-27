8fd6b9d89cde0c22cc1cefd44c923fd58c9e89da
liuziyuan <liuziyuan.room@gmail.com>
2024-03-27 19:38:14 +0800

    修改代码结构

--- retrofit-extension/retrofit-extension-sentinel/retrofit-extension-sentinel-core/src/main/java/io/github/liuziyuan/retrofit/extension/sentinel/core/BaseFallBack.java
+++
@@ -1,9 +0,0 @@
-package io.github.liuziyuan.retrofit.extension.sentinel.core;
-
-import io.github.liuziyuan.retrofit.core.exception.RetrofitExtensionException;
-
-public abstract class BaseFallBack<T extends RetrofitExtensionException> {
-
-    protected abstract void getFallBackException(T e);
-
-}
