a6967d7dc5d962dfd199d6efba996040487b7780
liuziyuan <liuziyuan.room@gmail.com>
2024-04-08 15:59:39 +0800

    reactor

---
+++ retrofit-adapters/reactor/src/main/java/io/github/liuziyuan/retrofit/adapter/reactor/ReactorCallAdapter.java
@@ -0,0 +1,78 @@
+/*
+ * Copyright (C) 2016 Jake Wharton
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.github.liuziyuan.retrofit.adapter.reactor;
+
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.FluxSink;
+import reactor.core.scheduler.Scheduler;
+import retrofit2.Call;
+import retrofit2.CallAdapter;
+import retrofit2.Response;
+
+import java.lang.reflect.Type;
+import java.util.function.Consumer;
+
+import static reactor.core.publisher.FluxSink.OverflowStrategy.LATEST;
+
+final class ReactorCallAdapter<R> implements CallAdapter<R, Object> {
+    private final Type responseType;
+    private final Scheduler scheduler;
+    private final boolean isAsync;
+    private final boolean isResult;
+    private final boolean isBody;
+    private final boolean isMono;
+
+    ReactorCallAdapter(Type responseType, Scheduler scheduler, boolean isAsync, boolean isResult,
+                       boolean isBody, boolean isMono) {
+        this.responseType = responseType;
+        this.scheduler = scheduler;
+        this.isAsync = isAsync;
+        this.isResult = isResult;
+        this.isBody = isBody;
+        this.isMono = isMono;
+    }
+
+    @Override
+    public Type responseType() {
+        return responseType;
+    }
+
+    @Override
+    public Object adapt(Call<R> call) {
+        Consumer<FluxSink<Response<R>>> consumer = isAsync
+                ? new EnqueueSinkConsumer<>(call)
+                : new ExecuteSinkConsumer<>(call);
+        Flux<Response<R>> responseFlux = Flux.create(consumer, LATEST);
+
+        Flux<?> flux;
+        if (isResult) {
+            flux = new ResultFlux<>(responseFlux);
+        } else if (isBody) {
+            flux = new BodyFlux<>(responseFlux);
+        } else {
+            flux = responseFlux;
+        }
+
+        if (scheduler != null) {
+            flux = flux.subscribeOn(scheduler);
+        }
+
+        if (isMono) {
+            return flux.single();
+        }
+        return flux;
+    }
+}
---
+++ retrofit-adapters/reactor/src/test/java/io/github/liuziyuan/retrofit/adapter/reactor/TestScheduler.java
@@ -0,0 +1,39 @@
+package io.github.liuziyuan.retrofit.adapter.reactor;
+
+import reactor.core.Disposable;
+import reactor.core.scheduler.Scheduler;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.List;
+
+final class TestScheduler implements Scheduler {
+  private final Deque<Runnable> tasks = new ArrayDeque<>();
+
+  void triggerActions() {
+    while (!tasks.isEmpty()) {
+      tasks.removeFirst().run();
+    }
+  }
+
+  @Override public Disposable schedule(Runnable task) {
+    return createWorker().schedule(task);
+  }
+
+  @Override public Worker createWorker() {
+    return new Worker() {
+      private final List<Runnable> workerTasks = new ArrayList<>();
+
+      @Override public Disposable schedule(Runnable task) {
+        workerTasks.add(task);
+        tasks.add(task);
+        return () -> tasks.remove(task);
+      }
+
+      @Override public void dispose() {
+        tasks.removeAll(workerTasks);
+      }
+    };
+  }
+}
