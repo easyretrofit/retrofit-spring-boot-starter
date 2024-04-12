6a2195f0e440371498e2c6eee9a6981412270a24
liuziyuan <liuziyuan.room@gmail.com>
2024-04-12 13:01:15 +0800

    fix issue

--- retrofit-integration/retrofit-spring-boot-reactor-starter/src/main/java/io/github/liuziyuan/retrofit/spring/boot/reactor/RxJava3CallAdapterFactoryBuilder.java
+++ retrofit-integration/retrofit-spring-boot-reactor-starter/src/main/java/io/github/liuziyuan/retrofit/spring/boot/reactor/RxJava3CallAdapterFactoryBuilder.java
@@ -1,6 +1,5 @@
 package io.github.liuziyuan.retrofit.spring.boot.reactor;
 
-import com.jakewharton.retrofit2.adapter.reactor.ReactorCallAdapterFactory;
 import io.github.liuziyuan.retrofit.core.builder.BaseCallAdapterFactoryBuilder;
 import retrofit2.CallAdapter;
 import retrofit2.adapter.rxjava3.RxJava3CallAdapterFactory;
