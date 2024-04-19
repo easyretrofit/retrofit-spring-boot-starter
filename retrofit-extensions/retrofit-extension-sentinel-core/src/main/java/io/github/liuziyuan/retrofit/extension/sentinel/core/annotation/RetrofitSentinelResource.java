35961ea7e6776c136adb80f708671a4bb7008e7c
liuziyuan <liuziyuan.room@gmail.com>
2024-04-19 13:44:16 +0800

    fix sentinel issue

--- retrofit-extensions/retrofit-extension-sentinel-core/src/main/java/io/github/liuziyuan/retrofit/extension/sentinel/core/annotation/RetrofitSentinelResource.java
+++ retrofit-extensions/retrofit-extension-sentinel-core/src/main/java/io/github/liuziyuan/retrofit/extension/sentinel/core/annotation/RetrofitSentinelResource.java
@@ -15,6 +15,7 @@ import java.lang.annotation.*;
 @Documented
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
+@RetrofitInterceptor(handler = RetrofitSentinelInterceptor.class)
 public @interface RetrofitSentinelResource {
 
     /**
