6919fa9a7c8c945425bb486c826bf0d0ad545a4a
liuziyuan <liuziyuan.room@gmail.com>
2024-05-09 10:10:55 +0800

    add RetrofitExtension annotation

---
+++ retrofit-core/src/main/java/io/github/liuziyuan/retrofit/core/annotation/RetrofitExtension.java
@@ -0,0 +1,9 @@
+package io.github.liuziyuan.retrofit.core.annotation;
+
+import java.lang.annotation.*;
+
+@Documented
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ ElementType.TYPE })
+public @interface RetrofitExtension {
+}
