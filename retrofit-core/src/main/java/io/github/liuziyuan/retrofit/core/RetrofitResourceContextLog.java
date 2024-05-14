c87fbea366aa046c9fc8fc61d351c8ca6aef87a2
liuziyuan <liuziyuan.room@gmail.com>
2024-05-14 17:23:43 +0800

    modify

---
+++ retrofit-core/src/main/java/io/github/liuziyuan/retrofit/core/RetrofitResourceContextLog.java
@@ -0,0 +1,60 @@
+package io.github.liuziyuan.retrofit.core;
+
+import io.github.liuziyuan.retrofit.core.resource.RetrofitApiServiceBean;
+import io.github.liuziyuan.retrofit.core.resource.RetrofitBuilderBean;
+import io.github.liuziyuan.retrofit.core.resource.RetrofitClientBean;
+import io.github.liuziyuan.retrofit.core.resource.UrlStatus;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+@Slf4j
+public class RetrofitResourceContextLog {
+
+    private final RetrofitResourceContext context;
+
+    public RetrofitResourceContextLog(RetrofitResourceContext context) {
+        this.context = context;
+    }
+
+    public void showLog() {
+        for (RetrofitClientBean retrofitClient : context.getRetrofitClients()) {
+            final String retrofitInstanceName = retrofitClient.getRetrofitInstanceName();
+            final String realHostUrl = retrofitClient.getRealHostUrl();
+            if (retrofitClient.getUrlStatus().equals(UrlStatus.DYNAMIC_URL_ONLY)) {
+                log.warn("---Retrofit Client : HostURL[Dummy]: {}, Retrofit instance name: {}", realHostUrl, retrofitInstanceName);
+            } else {
+                log.info("---Retrofit Client : HostURL: {}, Retrofit instance name: {}", realHostUrl, retrofitInstanceName);
+            }
+            retrofitClientDebugLog(retrofitClient);
+            for (RetrofitApiServiceBean retrofitService : retrofitClient.getRetrofitApiServiceBeans()) {
+                final Class<?> selfClazz = retrofitService.getSelfClazz();
+                final Class<?> parentClazz = retrofitService.getParentClazz();
+                String parentClazzName = null;
+                if (!parentClazz.getName().equals(selfClazz.getName())) {
+                    parentClazzName = parentClazz.getName();
+                }
+                log.info("|--API Services: Interface name: {} , Parent Interface name: {}", selfClazz.getName(), parentClazzName);
+            }
+        }
+    }
+
+    private void retrofitClientDebugLog(RetrofitClientBean retrofitClient) {
+        final String realHostUrl = retrofitClient.getRealHostUrl();
+        RetrofitBuilderBean retrofitBuilder = retrofitClient.getRetrofitBuilder();
+        final String globalEnable = retrofitBuilder.isEnable() ? "true" : "false";
+        String CallAdapterFactoryString = StringUtils.join(Arrays.stream(retrofitBuilder.getAddCallAdapterFactory()).map(Class::getSimpleName).collect(Collectors.toList()), ",");
+        String ConverterFactoryString = StringUtils.join(Arrays.stream(retrofitBuilder.getAddConverterFactory()).map(Class::getSimpleName).collect(Collectors.toList()), ",");
+        String callbackExecutorString = retrofitBuilder.getCallbackExecutor().getSimpleName();
+        String clientString = retrofitBuilder.getClient().getSimpleName();
+        String callFactoryString = retrofitBuilder.getCallFactory().getSimpleName();
+        String validateEagerlyString = retrofitBuilder.isValidateEagerly() ? "true" : "false";
+        String inheritedInterceptor = retrofitClient.getInheritedInterceptors().toString();
+        String interceptor = retrofitClient.getInterceptors().toString();
+        log.debug("RetrofitClientBean: HostURL: {}; UrlStatus: {}; globalEnable: {}; CallAdapterFactory: {}; ConverterFactory:{}; callbackExecutor: {}; client: {}; callFactory: {}; validateEagerly: {}; inheritedInterceptor: {}; interceptor: {}",
+                realHostUrl, retrofitClient.getUrlStatus(), globalEnable, CallAdapterFactoryString, ConverterFactoryString, callbackExecutorString, clientString, callFactoryString, validateEagerlyString, inheritedInterceptor, interceptor);
+
+    }
+}
--- retrofit-spring-boot-starter/src/main/java/io/github/liuziyuan/retrofit/spring/boot/RetrofitResourceDefinitionRegistry.java
+++ retrofit-spring-boot-starter/src/main/java/io/github/liuziyuan/retrofit/spring/boot/RetrofitResourceDefinitionRegistry.java
@@ -185,43 +185,10 @@ public class RetrofitResourceDefinitionRegistry implements BeanDefinitionRegistr
                 this.getClass().getPackage().getImplementationVersion(),
                 "v2.9.0");
 
-        for (RetrofitClientBean retrofitClient : context.getRetrofitClients()) {
-            final String retrofitInstanceName = retrofitClient.getRetrofitInstanceName();
-            final String realHostUrl = retrofitClient.getRealHostUrl();
-            if (retrofitClient.getUrlStatus().equals(UrlStatus.DYNAMIC_URL_ONLY)) {
-                log.warn("---Retrofit Client : HostURL[Dummy]: {}, Retrofit instance name: {}", realHostUrl, retrofitInstanceName);
-            } else {
-                log.info("---Retrofit Client : HostURL: {}, Retrofit instance name: {}", realHostUrl, retrofitInstanceName);
-            }
-            retrofitClientDebugLog(retrofitClient);
-            for (RetrofitApiServiceBean retrofitService : retrofitClient.getRetrofitApiServiceBeans()) {
-                final Class<?> selfClazz = retrofitService.getSelfClazz();
-                final Class<?> parentClazz = retrofitService.getParentClazz();
-                String parentClazzName = null;
-                if (!parentClazz.getName().equals(selfClazz.getName())) {
-                    parentClazzName = parentClazz.getName();
-                }
-                log.info("|--API Services: Interface name: {} , Parent Interface name: {}", selfClazz.getName(), parentClazzName);
-            }
-        }
+        RetrofitResourceContextLog retrofitResourceContextLog = new RetrofitResourceContextLog(context);
+        retrofitResourceContextLog.showLog();
     }
 
-    private void retrofitClientDebugLog(RetrofitClientBean retrofitClient) {
-        final String realHostUrl = retrofitClient.getRealHostUrl();
-        RetrofitBuilderBean retrofitBuilder = retrofitClient.getRetrofitBuilder();
-        final String globalEnable = retrofitBuilder.isEnable() ? "true" : "false";
-        String CallAdapterFactoryString = StringUtils.join(Arrays.stream(retrofitBuilder.getAddCallAdapterFactory()).map(Class::getSimpleName).collect(Collectors.toList()), ",");
-        String ConverterFactoryString = StringUtils.join(Arrays.stream(retrofitBuilder.getAddConverterFactory()).map(Class::getSimpleName).collect(Collectors.toList()), ",");
-        String callbackExecutorString = retrofitBuilder.getCallbackExecutor().getSimpleName();
-        String clientString = retrofitBuilder.getClient().getSimpleName();
-        String callFactoryString = retrofitBuilder.getCallFactory().getSimpleName();
-        String validateEagerlyString = retrofitBuilder.isValidateEagerly() ? "true" : "false";
-        String inheritedInterceptor = retrofitClient.getInheritedInterceptors().toString();
-        String interceptor = retrofitClient.getInterceptors().toString();
-        log.debug("RetrofitClientBean: HostURL: {}; UrlStatus: {}; globalEnable: {}; CallAdapterFactory: {}; ConverterFactory:{}; callbackExecutor: {}; client: {}; callFactory: {}; validateEagerly: {}; inheritedInterceptor: {}; interceptor: {}",
-                realHostUrl, retrofitClient.getUrlStatus(), globalEnable, CallAdapterFactoryString, ConverterFactoryString, callbackExecutorString, clientString, callFactoryString, validateEagerlyString, inheritedInterceptor, interceptor);
-
-    }
 
     @Override
     public void setEnvironment(Environment environment) {
