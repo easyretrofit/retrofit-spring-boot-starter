2fcea7a1ef77cd0c15c7baa6cfcce439a5f385ad
liuziyuan <liuziyuan.room@gmail.com>
2024-05-31 19:05:26 +0800

    fix abstract scope issue

--- retrofit-core/src/main/java/io/github/liuziyuan/retrofit/core/proxy/BaseFallBack.java
+++ retrofit-core/src/main/java/io/github/liuziyuan/retrofit/core/proxy/BaseFallBack.java
@@ -4,7 +4,7 @@ import io.github.liuziyuan.retrofit.core.exception.RetrofitExtensionException;
 
 public abstract class BaseFallBack<T extends RetrofitExtensionException> {
 
-    abstract void setFallBackException(T e);
+    protected abstract void setFallBackException(T e);
 
     public void setException(T exception) {
         setFallBackException(exception);
